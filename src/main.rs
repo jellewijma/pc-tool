63940510871e44667d821b3fc795e1e771ebd476
Jelle <jelle.wijma@gmail.com>
2023-12-13 17:42:33 +0100

    v0.1.1 done (custom ping)

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,108 @@
-use iced::{executor, Application, Command, Element, Settings, Text};
+use iced::{
+    button, text_input, Application, Button, Clipboard, Column, Command, Element, Text, TextInput,
+};
 use std::process::Command as Cmd;
 use std::str;
 
 #[derive(Debug, Clone)]
+struct ClonableIoError {
+    description: String,
+}
+
+impl From<std::io::Error> for ClonableIoError {
+    fn from(error: std::io::Error) -> Self {
+        ClonableIoError {
+            description: error.to_string(),
+        }
+    }
+}
+
+// Now you can use `ClonableIoError` in your `Result` type:
+#[derive(Debug, Clone)]
 enum Message {
-    PingLoaded(Result<String, std::io::Error>),
+    PingIpChanged(String),
+    PingButtonPressed,
+    PingLoaded(Result<String, ClonableIoError>),
 }
 
 struct Gui {
+    ping_ip: String,
+    ping_ip_state: text_input::State,
+    ping_button: button::State,
     ping: Option<String>,
 }
 
-impl Application for Gui {
-    type Executor = executor::Default;
+impl iced::Application for Gui {
+    type Executor = iced::executor::Default;
     type Message = Message;
     type Flags = ();
 
     fn new(_flags: ()) -> (Gui, Command<Message>) {
-        (Gui { ping: None }, Command::perform(load_ping(), Message::PingLoaded))
+        (
+            Gui {
+                ping_ip: String::new(),
+                ping_ip_state: text_input::State::new(),
+                ping_button: button::State::new(),
+                ping: None,
+            },
+            Command::none(),
+        )
     }
 
     fn title(&self) -> String {
         String::from("Network Ping")
     }
 
-    fn update(&mut self, message: Message) -> Command<Message> {
-        match message {
-            Message::PingLoaded(result) => {
-                self.ping = result.ok();
-            }
-        }
-        Command::none()
-    }
+    fn view(&mut self) -> Element<Message> {
+        let input = TextInput::new(
+            &mut self.ping_ip_state,
+            "Enter IP...",
+            &self.ping_ip,
+            Message::PingIpChanged,
+        );
 
-    fn view(&self) -> Element<Message> {
+        let button = Button::new(&mut self.ping_button, Text::new("Ping"))
+            .on_press(Message::PingButtonPressed);
+
         let content = match &self.ping {
             Some(ping) => format!("Ping: {}", ping),
             None => String::from("Loading..."),
         };
-        Text::new(content).into()
+
+        Column::new()
+            .push(input)
+            .push(button)
+            .push(Text::new(content))
+            .into()
+    }
+
+    fn update(&mut self, message: Message, _clipboard: &mut Clipboard) -> Command<Message> {
+        match message {
+            Message::PingIpChanged(ping_ip) => {
+                self.ping_ip = ping_ip;
+            }
+            Message::PingButtonPressed => {
+                return Command::perform(load_ping(self.ping_ip.clone()), Message::PingLoaded);
+            }
+            Message::PingLoaded(Ok(ping)) => {
+                self.ping = Some(ping);
+            }
+            Message::PingLoaded(Err(error)) => {
+                self.ping = Some(format!("Error: {}", error.description));
+            }
+        }
+
+        Command::none()
     }
 }
 
-async fn load_ping() -> Result<String, std::io::Error> {
+async fn load_ping(ip: String) -> Result<String, ClonableIoError> {
     let output = Cmd::new("ping")
         .arg("-c")
         .arg("1")
-        .arg("8.8.8.8") // You can replace this with the IP you want to ping
-        .output()?;
+        .arg(ip)
+        .output()
+        .map_err(ClonableIoError::from)?;
 
     let output_str = str::from_utf8(&output.stdout).unwrap();
     let ping_time_line = output_str
@@ -66,5 +121,5 @@ async fn load_ping() -> Result<String, std::io::Error> {
 }
 
 fn main() -> iced::Result {
-    Gui::run(Settings::default())
+    Gui::run(iced::Settings::default())
 }
